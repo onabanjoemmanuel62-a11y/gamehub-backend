e13c59c86773056fdf1055bc90815cd88cb70aa5
Emmanuel <onabanjoemmanuel62@gmail.com>
2025-10-15 07:21:57 +0100

    Fix order status update - remove parseInt and update session config

--- server.js
+++ server.js
@@ -20,10 +20,12 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: false, // set to true if using HTTPS
+    secure: false,
     httpOnly: true,
-    maxAge: 24 * 60 * 60 * 1000 // 24 hours
-  }
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+    sameSite: 'lax'
+  },
+  proxy: true // Important for Render
 }));
 
 // Simple health check
